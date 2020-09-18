c86448998e833027587c6f749213ee57d22ce733
GerHanNaAn <nakhtigal.german@gmail.com>
2020-09-18 17:52:08 +0500

    popup

--- src/js/main.js
+++ src/js/main.js
@@ -9,23 +9,41 @@ document.querySelector(".burger-wrap").onmouseover = function () {
 const burger = document.querySelector("#burger");
 const mobileMenu = document.querySelector("#mobileMenu");
 const cross = document.querySelector("#mobileMenuCross");
-const backg = document.querySelector("#mobMBg");
+const overlay = document.querySelector("#mobMBg");
 const body = document.querySelector("body");
 
+function DetectMobile() {
+  let uagent = navigator.userAgent.toLowerCase();
+  if (uagent.search("mobile") == -1) body.classList.toggle("hideScrl");
+}
 function burgerMenu(e) {
   if (e.target === burger) {
     burger.classList.toggle("menu-icon_active");
   }
   mobileMenu.classList.toggle("mobile-menu_active");
-  backg.classList.toggle("mobile-menu__bg_active");
+  overlay.classList.toggle("overlay_active");
   body.classList.toggle("lock");
-  function DetectMobile() {
-    let uagent = navigator.userAgent.toLowerCase();
-    if (uagent.search("mobile") == -1) body.classList.toggle("hideScrl");
-  }
   DetectMobile();
 }
 
 burger.addEventListener("click", burgerMenu);
 cross.addEventListener("click", burgerMenu);
-backg.addEventListener("click", burgerMenu);
+overlay.addEventListener("click", burgerMenu);
+
+// Popup
+const btnPopup = document.querySelector("#contacts");
+const popup = document.querySelector("#popupForm");
+const popupClose = document.querySelector("#popupClose");
+const overlay2 = document.querySelector("#overlay2");
+
+function popupActions(e) {
+  popup.classList.toggle("popup_active");
+  overlay2.classList.toggle("overlay_active");
+  body.classList.toggle("lock");
+  DetectMobile();
+  console.log(e.target);
+}
+
+btnPopup.addEventListener("click", popupActions);
+popupClose.addEventListener("click", popupActions);
+overlay2.addEventListener("click", popupActions);
