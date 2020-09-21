cc82abdcaa7efb9bf05557604ca3f962e29160e7
GerHanNaAn <nakhtigal.german@gmail.com>
2020-09-22 00:09:04 +0500

    ajax

--- src/js/main.js
+++ src/js/main.js
@@ -41,9 +41,50 @@ function popupActions(e) {
   overlay2.classList.toggle("overlay_active");
   body.classList.toggle("lock");
   DetectMobile();
-  console.log(e.target);
 }
 
 btnPopup.addEventListener("click", popupActions);
 popupClose.addEventListener("click", popupActions);
 overlay2.addEventListener("click", popupActions);
+
+// AJAX
+const form = document.querySelector("#popupForm");
+const popupForm = document.querySelector(".popup__form");
+const popupDone = document.querySelector(".popup__text");
+
+form.addEventListener("submit", function (evt) {
+  evt.preventDefault();
+
+  const formData = {
+    name: document.querySelector("#name").value,
+    email: document.querySelector("#email").value,
+  };
+
+  const request = new XMLHttpRequest();
+
+  function popupTogle() {
+    popupForm.classList.toggle("popup__item_active");
+    popupDone.classList.toggle("popup__item_active");
+  }
+
+  request.addEventListener("load", function () {
+    // В этой части кода можно обрабатывать ответ от сервера
+    console.log(request.response);
+    popupActions();
+    setTimeout(popupTogle, 600);
+    // popupActions();
+    setTimeout(popupActions, 700);
+  });
+
+  request.open("POST", "", true);
+  request.setRequestHeader(
+    "Content-Type",
+    "application/x-www-form-urlencoded; charset=UTF-8"
+  );
+  request.send(
+    "name=" +
+      encodeURIComponent(formData.name) +
+      "&email=" +
+      encodeURIComponent(formData.email)
+  );
+});
---
+++ src/views/send.php
@@ -0,0 +1,15 @@
+<?php
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+
+$to = "nakhtigal.german@gmail.com";
+$subject ='Отправлена заявка с сайта НАЗВАНИЕ_САЙТА';
+$message = "Заявка была отправлена пользоваталем $name с имейлом $email";
+$headers = "From: $name <$email>" . "\r\n";
+
+if (mail($to,$subject,$message,$headers)) {
+	echo 'Ваше сообщение успешно отправлено (ответ сервера)!';
+} else {
+	echo 'Возникла ошибка при отработке функции mail на сервере (ответ сервера)!';
+}
